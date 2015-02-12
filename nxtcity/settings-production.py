c568dd374c5b6606a825752ec83ad46ea2f59ffd
mslatour <sanderlatour@gmail.com>
2015-02-12 11:20:10 +0100

    Removed setting of x-frame-options
    The reason for this is that the DNS provider uses frames to support redirection
    of bare root domains to a hostname.

--- nxtcity/settings-production.py
+++ nxtcity/settings-production.py
@@ -50,7 +50,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'nxtcity.urls'
