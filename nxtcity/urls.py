49613056d3f953fd4eaea5cdd38c10b3b11bb9bc
mslatour <sanderlatour@gmail.com>
2015-02-20 13:57:00 +0100

    Added language toggle buttons.

--- nxtcity/urls.py
+++ nxtcity/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 import landing.views as landing
 
 urlpatterns = patterns('',
-    url(r'^/?$', landing.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += i18n_patterns('',
+    url(r'^/?$', landing.home, name='home'),
 )
