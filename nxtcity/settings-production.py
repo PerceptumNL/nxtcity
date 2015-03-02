c92db5422b2619568488505b1f2c57b856a4134a
mslatour <sanderlatour@gmail.com>
2015-03-02 11:53:31 +0100

    Added email settings for production

--- nxtcity/settings-production.py
+++ nxtcity/settings-production.py
@@ -160,3 +160,9 @@ WAGTAIL_SITE_NAME = "NxtCity Amsterdam 2015"
 # Whether to use face/feature detection to improve image cropping - requires OpenCV
 WAGTAILIMAGES_FEATURE_DETECTION_ENABLED = False
 
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+SERVER_EMAIL = 'nxtcity-platform@perceptum.nl'
