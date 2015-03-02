2eebac2b0a16a55e33094159d5fc0ed132aedae4
mslatour <sanderlatour@gmail.com>
2015-03-02 20:40:44 +0100

    Added a blank page; generalized background colors
    Test Plan: Run
    
    Reviewers: eszter
    
    Differential Revision: http://review.prcptm.nl/D4

--- core/models.py
+++ core/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
 
 
 class HomePage(Page):
     pass
+
+class BlankPage(Page):
+    body = RichTextField()
+
+BlankPage.content_panels = [
+    FieldPanel('title'),
+    FieldPanel('body')
+]
