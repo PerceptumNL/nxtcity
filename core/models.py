edc742a11a25e65db1964100e30e20bef7343b41
Eszter Fodor <chytos@gmail.com>
2015-03-04 10:52:08 +0100

    Nothing really changed yet

--- core/models.py
+++ core/models.py
@@ -15,3 +15,16 @@ BlankPage.content_panels = [
     FieldPanel('title'),
     FieldPanel('body')
 ]
+
+class BlogPage(Page):
+    body = RichTextField()
+    date = models.DateField("Post date")
+    search_name = "Blog Page"
+
+    indexed_fields = ('body', )
+
+BlogPage.content_panels = [
+    FieldPanel('title', classname = "full title"),
+    FieldPanel('date'),
+    FieldPanel('body', classname = "full"),
+]
